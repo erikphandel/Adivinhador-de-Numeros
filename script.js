5fd0f798e9d43504372dc3d64a721017640cf8c4
Erik Handel <erikphandel@gmail.com>
2025-07-08 17:25:18 -0300

    Primeiro upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const numeroSecreto = gerarNumero(1,100)
+function gerarNumero(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + 1;
+}
+console.log(numeroSecreto)
+
+// Funções utilitárias
+const obterElemento = (id)  => document.getElementById(id);
+const tentativas = parseFloat(obterElemento('tentativas').value);
+console.log(tentativas)
+
+// Obter valor inputado
+const obterNumero = () => parseInt(obterElemento('numero').value);
+
+// Comparar valor inputado com número secreto
+function compararNumero () {
+    if (numeroSecreto = obterNumero) {
+        document.getElementById("mensagens")
+    }
+}
+
+
+
+// PASSOS
+// 1. Gerar numero secreto
+// 2. Obter número inputado pelo usuário
+// 3. Comparar número inputado com número secreto
+// 4. Se número inputador for número secreto, emitir mensagem de vitória
+// 5. Se número inputado não for número secreto, emitir mensagem de erro
+// 5.1 Diminuir número de tentativas.
+// 6. Caso o número de tentativas acabe, emitir mensagem de derrota e revelar número secreto.
