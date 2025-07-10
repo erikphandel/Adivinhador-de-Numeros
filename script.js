2428ea74f12ec4a92a7c25563785865224980c11
Erik Handel <erikphandel@gmail.com>
2025-07-10 17:57:05 -0300

    Comentários e correções

--- script.js
+++ script.js
@@ -1,67 +1,55 @@
 // Gerar Número Secreto
-const numeroSecreto = gerarNumero(1,100)
+const numeroSecreto = gerarNumero(1,100)// Variável contendo o Número Secreto, que é o objetivo do jogo.
 function gerarNumero(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + 1;
-};
-console.log("O número secreto é:", numeroSecreto);
-let tentativas = 3;
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}; // Math.random gera um número entre 0 e 1. A multiplicação gera um número entre 0 e 99,9999.... Math.floor arredonda o número gerado para baixo (0 a 99). + 1 garante que o número gerado é de 1 a 100.
+console.log("O número secreto é:", numeroSecreto); // Para debug apenas
+
 // Funções utilitárias
-const obterElemento = (id)  => document.getElementById(id);
-let mensagem = obterElemento('mensagem');
-obterElemento ('tentativas').textContent = tentativas
-let jogoFinalizado = false
+let tentativas = 3; // Variável com o número de tentativas restantes.
+const obterElemento = (id)  => document.getElementById(id); // Funçao para abreviar a função getElementeById()
+let mensagem = obterElemento('mensagem'); // Variável contendo a mensagem a ser escrita quando o usuário faz um chute
+obterElemento ('tentativas').textContent = tentativas // Comando que faz com que o número de tentativas exibido no HTML possa ser atualizado
+let jogoFinalizado = false // Variável que determina se o jogo acabou ou não. Quando o jogo acaba, o botão "Jogar Novamente" é exibido
 function mostrarBotaoReinicio() {
     obterElemento('reiniciar').style.display = 'inline';
-}
+} // Função que mostra o botão de reinicar o jogo, que é carregado escondido na página inicial.
 function reiniciarJogo() {
     window.location.reload();
-}
+} // Função que recarrega a página quando o botão "Jogar novamente" é clicado
 
+function obterNumero () {// Essa função é chamada quando o botão "Chutar" é clicado.
 
-// Obter valor inputado
-function obterNumero () {
-    if (jogoFinalizado) return
+    if (jogoFinalizado) return // Se jogoFinalizado for true, a função não roda.
 
-    const numeroInputado = parseInt(obterElemento('numero').value);
+    const numeroInputado = parseInt(obterElemento('numero').value); // A variável numeroInputado é obtida pelo número inputado pelo usuário
     if (numeroInputado < 1 || numeroInputado > 100 || isNaN(numeroInputado)) {
     mensagem.textContent = "Por favor, digite um número entre 1 e 100.";
-    return;
+    return; // Se numeroInputado não for um número de 1 a 100, exibe uma mensagem de erro e a função não roda.
 }
 
-    if (numeroInputado === numeroSecreto) {
-        mensagem.textContent = "Parabéns! Você adivinhou o número secreto. Atualize a página para jogar de novo.";
+    // Esse if/else analisa se numeroInputado é igual, menor ou maior que numeroSecreto
+    if (numeroInputado === numeroSecreto) {// Se o jogador adivinhar, termina o jogo, desabilitando o botão de chute.
+        mensagem.textContent = "Parabéns! Você adivinhou o número secreto.";
         jogoFinalizado = true;
+        obterElemento('numero').disabled = true;
+        obterElemento('botao').disabled = true;
     } else if (numeroInputado < numeroSecreto) {
         mensagem.textContent = "O número secreto é maior do que isso!";
     } else {
         mensagem.textContent = "O número secreto é menor do que isso!";
     };
 
-    tentativas--;
-    obterElemento ('tentativas').textContent = tentativas;
+    tentativas--;// Diminui tentativas em 1
+    obterElemento ('tentativas').textContent = tentativas;// Atualiza o número de tentativas no HTML
     
+    // Se o jogo chegar em 0 tentativas e não estiver finalizado, exibe mensagem de derrota e finaliza o jogo, desabilitando o botão de chute.
     if (tentativas === 0 && !jogoFinalizado) {
-        mensagem.textContent = "Você perdeu! O número secreto era " + numeroSecreto + ". Atualize a página para jogar de novo.";
+        mensagem.textContent = "Você perdeu! O número secreto era " + numeroSecreto;
         jogoFinalizado = true;
         obterElemento('numero').disabled = true;
         obterElemento('botao').disabled = true;
     }
 
-    if (jogoFinalizado) mostrarBotaoReinicio ();
+    if (jogoFinalizado) mostrarBotaoReinicio ();// Se o jogo estiver finalizado, mostra o botão "Jogar novamente"
 };
-
-
-
-
-
-
-
-
-// PASSOS
-// 1. Gerar numero secreto - CONCLUIDO
-// 2. Obter número inputado pelo usuário - CONCLUÍDO
-// 3. Comparar número inputado com número secreto
-// 4. Se número inputado for número secreto, emitir mensagem de vitória
-// 5. Se número inputado não for número secreto, emitir mensagem de erro
-// 5.1 Diminuir número de tentativas.
-// 6. Caso o número de tentativas acabe, emitir mensagem de derrota e revelar número secreto.
